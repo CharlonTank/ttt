b2ca05a82307638e343f79727d4ed515a62ec974
Charlon <charles.assus@gmail.com>
2024-12-16 22:08:19 +0100

    Preparing for v17

---
+++ src/Evergreen/V17/Tutorial/Types.elm
@@ -0,0 +1,9 @@
+module Evergreen.V17.Tutorial.Types exposing (..)
+
+
+type TutorialStep
+    = TutorialBasicMove
+    | TutorialBoardSelection
+    | TutorialWinningSmall
+    | TutorialFreeChoice
+    | TutorialWinningBig
