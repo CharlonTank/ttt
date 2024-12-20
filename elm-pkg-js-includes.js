366c89da2e67eb0d24030bcc8ade1f2c72eaebe3
Charlon <charles.assus@gmail.com>
2024-12-20 13:13:10 +0100

    go

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -1,5 +1,7 @@
 const local_storage = require('./elm-pkg-js/local-storage')
+const audio = require('./elm-pkg-js/audio')
 
 exports.init = async function init(app) {
     local_storage.init(app)
+    audio.init(app)
 } 
